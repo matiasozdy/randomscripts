4afb7e7e53335087ff4dc5024c256eeff01efd55
mozdy <mozdy@mozdy.tango.leander>
2016-11-24 16:00:06 -0300

    clean info

--- check_dbmaxconns.sh
+++ check_dbmaxconns.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-OUTPUT=`PGPASSWORD="$1" check_postgres_backends --host=localhost --dbuser=lega-user --output=simple -w 95 -c 100`
+OUTPUT=`PGPASSWORD="$1" check_postgres_backends --host=localhost --dbuser=$2 --output=simple -w 95 -c 100`
 #EXITSTAT=$?
 #if [ $EXITSTAT -eq 0 ]; then
 #	echo "OK - $OUTPUT conns"
--- check_dbwraparound.sh
+++ check_dbwraparound.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-OUTPUT=`PGPASSWORD="$1" check_postgres_txn_wraparound --host=localhost --dbuser=lega-user --output=simple`
+OUTPUT=`PGPASSWORD="$1" check_postgres_txn_wraparound --host=localhost --dbuser=$2 --output=simple`
 #if [ $OUTPUT -gt 1900000000 ]; then
 #	echo "CRITICAL - $OUTPUT"
 #elif [ $OUTPUT -eq 1750000000 ]; then
